f12ca5f7dc00d88838bff4b847f6da2ba8a3a224
qinong-tianyaoqi <1174465110@qq.com>
2017-09-13 14:40:26 +0800

    Initial Commit

---
+++ MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by 田耀琦 on 2017/9/13.
+//  Copyright © 2017年 田耀琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM
+//
+//  Created by 田耀琦 on 2017/9/13.
+//  Copyright © 2017年 田耀琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
